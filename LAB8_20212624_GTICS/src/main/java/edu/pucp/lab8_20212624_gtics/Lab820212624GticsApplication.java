8f9d257ca7c838ca1bbae3e7c81be11203be0f79
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-05-30 20:14:22 -0500

    POM ACTUALIZADO

---
+++ LAB8_20212624_GTICS/src/main/java/edu/pucp/lab8_20212624_gtics/Lab820212624GticsApplication.java
@@ -0,0 +1,13 @@
+package edu.pucp.lab8_20212624_gtics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab820212624GticsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Lab820212624GticsApplication.class, args);
+    }
+
+}
---
+++ LAB8_20212624_GTICS/src/test/java/edu/pucp/lab8_20212624_gtics/Lab820212624GticsApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.pucp.lab8_20212624_gtics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab820212624GticsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
